c637baa89d126e0e777f331e37fe7ab6005e7e97
IdoHirsh0 <ido.hirsh.microsoft@gmail.com>
2022-04-16 16:01:17 +0300

    Prepended the bin path to the current session PATH

--- install.bat
+++ install.bat
@@ -6,6 +6,9 @@ cd scripts
 :: Add the bin directory path to the user's PATH environment variable
 powershell .\prepend_user_path.ps1
 
+:: Add the bin directory path to the current sessions PATH
+set PATH=%~dp0bin;%PATH%
+
 :: Return to the project directory
 cd ..
 
