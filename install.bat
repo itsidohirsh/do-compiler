2252b29bb05f09ab08d58cdbf6a100f3405906b2
IdoHirsh0 <ido.hirsh.microsoft@gmail.com>
2022-04-23 13:20:51 +0300

    Added bypass to execution policy for powershell scripts

--- install.bat
+++ install.bat
@@ -1,12 +1,10 @@
 @echo off
+cls
 
 :: Add the bin directory path to the user's PATH environment variable
 cd scripts
-powershell .\prepend_user_path.ps1
+powershell -ExecutionPolicy Bypass -File .\prepend_user_path.ps1
 cd ..
 
 :: Add the bin directory path to the current session PATH
 set PATH=%~dp0bin;%PATH%
-
-:: Clear the screen
-cls
--- uninstall.bat
+++ uninstall.bat
@@ -1,11 +1,11 @@
 @echo off
+cls
 
 :: Remove the bin directory path from the user's PATH environment variable
 cd scripts
-powershell .\remove_user_path.ps1
+powershell -ExecutionPolicy Bypass -File .\remove_user_path.ps1
 cd ..
 
 :: Delete the project
 cd ..
-cls
 goto 2>nul & rmdir /s /q %~dp0
